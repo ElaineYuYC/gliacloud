2fe924ab6e3fa3f0876b246dcb9d05c1c4606343
ElaineYuYC <30682914+ElaineYuYC@users.noreply.github.com>
2017-08-04 17:07:43 +0800

    counting
    exam 1st: counting

---
+++ counting.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+    listUrls = []
+    urls = ["http://www.google.com/a.txt", "http://www.google.com.tw/a.txt", "http://www.google.com/download/c.jpg", "http://www.google.co.jp/a.txt", "http://www.google.com/b.txt",
+"https://facebook.com/movie/b.txt", "http://yahoo.com/123/000/c.jpg", "http://gliacloud.com/haha.png",]
+    for strItem in urls:
+        intC = strItem.rfind('/')
+        listUrls.append(strItem[(intC+1):])
+    dicCount = {strE:listUrls.count(strE) for strE in listUrls}
+    lSort = sorted(dicCount,key=dicCount.__getitem__)
+    print(lSort[-1] + ' ' + str(dicCount[lSort[-1]]))
+    print(lSort[-2] + ' ' + str(dicCount[lSort[-2]]))
+    print(lSort[-3] + ' ' + str(dicCount[lSort[-3]]))  

66eb6557315c52c12da80ee94f35278107f2697a
ElaineYuYC <30682914+ElaineYuYC@users.noreply.github.com>
2017-08-04 17:59:26 +0800

    combination
    type error

---
+++ combination.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+sys.setrecursionlimit(5000)
+def factorial(a):
+    if a == 1:
+        return 1
+    else:
+        return a * factorial(a-1)
+
+if __name__ == '__main__':
+    n = input("n = ")
+    r = input("r = ")
+    n1 = factorial(int(n-1))
+    r1 = factorial(int(r))
+    r2 = factorial(int(n-r))
+    r3 = factorial(int(r-1))
+    c = (n1 / (r1*r2)) + (n1 / (r3*r2))
+    print(str(c))
